43d32bdbd24edc4752f661982c90fff2bc0610b6
Vagabond <Vagabond@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2016-04-01 23:24:04 +0000

    RisingFromTheAshes and EvacuationUnderFire: removed odasl from projects and added Outpost2Dialog project references. Fixed typo in EvacuationUnderFire mission briefing.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/LevelsAndMods/trunk/Levels/RisingFromTheAshes@1080 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

---
+++ BriefingDialog.cpp
@@ -0,0 +1,69 @@
+/* The following code will allow showing a modal dialog during a new scenario's initialization.
+   A resource script and resource.h are required that define the parameters of the modal dialog
+   and add the briefing text as a resource to the DLL scenario.*/
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+
+// Required includes for the mission briefing dialog
+#include <richedit.h>
+#include "resource.h"
+#include "odasl\odasl.h"
+
+extern HINSTANCE hInst; //HINSTANCE needs to be accessed from the dllMain function.
+LRESULT CALLBACK DialogProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+
+void ShowBriefing()
+{
+    // OP2 turns off the skinning before the game loads.
+    // We have to re-enable it (otherwise the briefing box will look ugly).
+    wplEnable();
+    // Show the dialog box, using the shell window as the parent
+	HWND shellWnd = FindWindow("Outpost2Shell", NULL);
+	DialogBoxParam(hInst, MAKEINTRESOURCE(IDD_MISSIONINFO), shellWnd, (DLGPROC)DialogProc, NULL);
+    // Turn off the skinning again (probably not needed but a good idea to do it anyway)
+    wplDisable();
+}
+
+LRESULT CALLBACK DialogProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    HRSRC txtRes = NULL;
+    HWND richEdit = NULL;
+	switch(msg)
+	{
+		case WM_INITDIALOG:
+            // Make the BG black in the rich edit box
+            richEdit = GetDlgItem(hWnd, IDC_TEXTAREA);
+            SendMessage(richEdit, EM_SETBKGNDCOLOR, 0, 0);
+
+            // Find the text resource
+            txtRes = FindResource(hInst, MAKEINTRESOURCE(IDR_MISSIONTEXT), "TEXT");
+            if (txtRes)
+            {
+                // Load it
+                HGLOBAL resHdl = LoadResource(hInst, txtRes);
+                if (resHdl)
+                {
+                    // Lock it and set the text
+                    char *briefing = (char*)LockResource(resHdl);
+                    if (briefing)
+                    {
+                        SetDlgItemText(hWnd, IDC_TEXTAREA, briefing);
+                        return TRUE;
+                    }
+                }
+            }
+            SetDlgItemText(hWnd, IDC_TEXTAREA, "Error: Mission text could not be loaded.");
+            return TRUE;
+        case WM_COMMAND:
+            if (wParam == IDOK)
+            {
+                EndDialog(hWnd, IDOK);
+                return TRUE;
+            }
+            return FALSE;
+	}
+	return FALSE;
+}
+
